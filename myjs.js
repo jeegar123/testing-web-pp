32bdf5e7339e5e235a3e7cc1f6bf3188d1e77d5b
Jeegar Vinodkumar <50761184+jeegarlearner@users.noreply.github.com>
2021-01-05 12:16:31 +0530

    js file updated

--- myjs.js
+++ myjs.js
@@ -1,5 +1,5 @@
 let h1 =document.getElementById("name");
 
-h1.addEventListener('mouseenter',()=>{
+h1.addEventListener('click',()=>{
     alert("welcome to Cybercom creation Trainning in Day-2");
 });
